34e309074440be5680b986399c92156e505cff5f
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-04-18 14:04:04 +0200

    save bdd

--- script_forum.sql
+++ script_forum.sql
@@ -2,7 +2,7 @@
 -- Hôte:                         127.0.0.1
 -- Version du serveur:           8.0.30 - MySQL Community Server - GPL
 -- SE du serveur:                Win64
--- HeidiSQL Version:             12.6.0.6765
+-- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -19,45 +19,60 @@
 CREATE DATABASE IF NOT EXISTS `lucie_forum` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `lucie_forum`;
 
--- Listage de la structure de la table lucie_forum. category
+-- Listage de la structure de table lucie_forum. category
 CREATE TABLE IF NOT EXISTS `category` (
   `id_category` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id_category`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table lucie_forum.category : ~3 rows (environ)
+INSERT INTO `category` (`id_category`, `name`) VALUES
+	(6, 'General'),
+	(7, 'Reviews'),
+	(8, 'Crowdfounding');
 
--- Listage de la structure de la table lucie_forum. post
+-- Listage de la structure de table lucie_forum. post
 CREATE TABLE IF NOT EXISTS `post` (
   `id_post` int NOT NULL AUTO_INCREMENT,
   `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `creationDate` date NOT NULL,
   `topic_id` int NOT NULL DEFAULT '0',
-  `user_id` int DEFAULT NULL,
+  `user_id` int NOT NULL,
   PRIMARY KEY (`id_post`) USING BTREE,
   KEY `topic_id` (`topic_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table lucie_forum.post : ~6 rows (environ)
+INSERT INTO `post` (`id_post`, `content`, `creationDate`, `topic_id`, `user_id`) VALUES
+	(13, 'Tell us what you think and if you experience any bug.', '2024-04-18', 11, 1),
+	(14, 'works fine for me!', '2024-04-18', 11, 8),
+	(15, 'I&#039;ve tried this game and here&#039;s why you should too: ', '2024-04-18', 12, 8),
+	(16, 'love it as well', '2024-04-18', 12, 9),
+	(17, 'I hate it', '2024-04-18', 13, 9),
+	(18, 'i thought it was ok', '2024-04-18', 13, 8);
 
--- Listage de la structure de la table lucie_forum. topic
+-- Listage de la structure de table lucie_forum. topic
 CREATE TABLE IF NOT EXISTS `topic` (
   `id_topic` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL DEFAULT '',
   `creationDate` date NOT NULL,
   `isLocked` tinyint NOT NULL DEFAULT '0',
   `category_id` int NOT NULL,
-  `user_id` int DEFAULT NULL,
+  `user_id` int NOT NULL,
   PRIMARY KEY (`id_topic`),
   KEY `category_id` (`category_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table lucie_forum.topic : ~3 rows (environ)
+INSERT INTO `topic` (`id_topic`, `title`, `creationDate`, `isLocked`, `category_id`, `user_id`) VALUES
+	(11, 'App has been updated', '2024-04-18', 0, 6, 1),
+	(12, 'Pagan is the best !!', '2024-04-18', 0, 7, 8),
+	(13, 'Vindication is the worst !!', '2024-04-18', 0, 7, 9);
 
--- Listage de la structure de la table lucie_forum. user
+-- Listage de la structure de table lucie_forum. user
 CREATE TABLE IF NOT EXISTS `user` (
   `id_user` int NOT NULL AUTO_INCREMENT,
   `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0',
@@ -67,9 +82,13 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id_user`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `nickname` (`nickname`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table lucie_forum.user : ~3 rows (environ)
+INSERT INTO `user` (`id_user`, `nickname`, `email`, `password`, `role`) VALUES
+	(1, 'lulu', 'lucie@exemple.com', '$2y$10$zIs2ZA.BuKpKwEreqC63Wu7xt2tuFDkQ6vrskk6QZhpPJXepxWW2K', '["ROLE_USER", "ROLE_ADMIN"]'),
+	(8, 'user 1', 'user1@exemple.com', '$2y$10$/FV62WEv1KUY.XJlTX3s2OudpFguwThIcUrD/vcn4cATtS0cRU7t.', '["ROLE_USER"]'),
+	(9, 'user2', 'user2@example.com', '$2y$10$tNMhzxtQkUt9RvZRGIeRq.nuQMM.CUYG8R/BFsIRBW/YsDlJLRhHa', '["ROLE_USER"]');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
